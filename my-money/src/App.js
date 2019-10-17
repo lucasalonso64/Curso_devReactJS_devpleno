2a669e861b9d49feb87dc4e45e772de5148ebb86
Lucas <alonsosistemas@gmail.com>
2019-10-17 17:04:39 -0300

    v2.3

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios'
 
-/*
-axios
-  .get('https://mymoney-alonsosistemas.firebaseio.com/valor.json')
-  .then(res => {
-    console.log(res)
-  }) */
+const url = 'https://mymoney-alonsosistemas.firebaseio.com/movimentacoes/2019-08.json'
+const reducer = (state, action) => {
+  console.log('state', state, 'action', action)
+  if (action.type === 'REQUEST'){
+    return {
+      ...state,
+      loading: true
+    }
+  }
+  if(action.type === 'SUCCESS'){
+    return {
+      ...state,
+      loading: false,
+      data: action.data
+    }
+  }
 
-  axios
-    .post('https://mymoney-alonsosistemas.firebaseio.com/valor.json', {
-      outro: 'LUcas ALonso'
-    })
-    .then(res => {
-      console.log(res)
-    })
-
+  return state
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, {
+    loading: true,
+    data: {}
+  })
+  useEffect(() => {
+    dispatch({ type: 'REQUEST' })
+    axios
+      .get(url)
+      .then(res => {
+        dispatch({ type: 'SUCCESS', data: res.data })
+      })
+
+  }, [])
+
   return (
     <div >
       <h1>My Money</h1>
-      
+      {JSON.stringify(data)}
+      {<p>Loading...</p>}
     </div>
   );
 }
