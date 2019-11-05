caf62022a642b0fbafde9cae3daaf59a0d18d334
Lucas <alonsosistemas@gmail.com>
2019-11-05 11:13:13 -0300

    V1.26

--- my-money/src/pages/Movimentacoes.js
+++ my-money/src/pages/Movimentacoes.js
@@ -1,15 +1,17 @@
 import React from 'react'
-
 import Rest from '../utils/rest'
 
 const baseURL = 'https://mymoney-alonsosistemas.firebaseio.com/'
 const { useGet } = Rest(baseURL)
 
-const Movimentacoes = ({ match }) => {
+const Movimentacoes = ( { match } ) => {
   const data = useGet(`movimentacoes/${match.params.data}`)
+
+
     return (
       <div className="container">
         <h1>Movimentações</h1>
+        
         <pre>{JSON.stringify(data)}</pre>
       </div>
 
