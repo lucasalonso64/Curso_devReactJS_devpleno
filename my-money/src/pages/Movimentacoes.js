3bf520b2e76d38687117082b29364eb0dd09f106
Lucas <alonsosistemas@gmail.com>
2019-11-14 16:56:22 -0300

    v1.27

--- my-money/src/pages/Movimentacoes.js
+++ my-money/src/pages/Movimentacoes.js
@@ -1,47 +1,75 @@
 import React from 'react'
+import { useState } from 'react'
 import Rest from '../utils/rest'
+
 
 
 
 //const baseURL = 'https://mymoney-lkk.firebaseio.com/';
 const baseURL = 'https://mymoney-alonsosistemas.firebaseio.com/'
-const {useGet} = Rest(baseURL);
+const { useGet, usePost } = Rest(baseURL);
 
+const Movimentacoes = ({ match }) => {
+  const data = useGet(`movimentacoes/${match.params.data}`)
+  const [postData, salvar ] = usePost(`movimentacoes/${match.params.data}`)
+  const [descricao, setDescricao] = useState('')
+  const [valor, setValor] = useState(0)
 
+  const onChangeDescricao = evt => {
+    setDescricao(evt.target.value)
+  }
 
+  const onChangeValor = evt => {
+    setValor(parseFloat(evt.target.value))
+  }
 
-const Movimentacoes = ({match}) => {
-  const data = useGet(`movimentacoes/${match.params.data}`);
+  const salvarMovimentacoes = () => {
+      salvar ({
+        descricao,
+        valor
+
+      })
+  }
+
   return (
-      <div className='container'>
-        <h1>Movimentações</h1>       
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>Descrição</th>
-              <th>Valor</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              data.data &&
-              Object
+    <div className='container'>
+      <h1>Movimentações</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.data &&
+            Object
               .keys(data.data)
               .map(movimentacao => {
                 return (
                   <tr>
                     <td>{data.data[movimentacao].descricao}</td>
-                    <td>{data.data[movimentacao].valor}</td>                    
+                    <td>{data.data[movimentacao].valor} </td>
                   </tr>
                 )
               })
-            }
-           
-          </tbody>
-        </table>
-      
-      </div>
-    );
+          }
+          <tr>
+            <td>
+              <input type='text' value={descricao} onChange={onChangeDescricao} />
+            </td>
+            <td>
+              <input type='text' value={valor} onChange={onChangeValor} />
+              <button onClick={salvarMovimentacoes}>+</button>
+            </td>
+          </tr>
+
+        </tbody>
+      </table>
+
+    </div>
+  );
 }
 
-  export default Movimentacoes;
+export default Movimentacoes;
