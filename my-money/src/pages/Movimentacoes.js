00843d5ab8ddcb95d5b00965a0f89d95376b60db
Lucas <alonsosistemas@gmail.com>
2019-11-14 12:55:38 -0300

    v1.27

--- my-money/src/pages/Movimentacoes.js
+++ my-money/src/pages/Movimentacoes.js
@@ -1,16 +1,17 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Rest from '../utils/rest'
 
-const baseURL = 'https://mymoney-alonsosistemas.firebaseio.com/'
+
+
 //const baseURL = 'https://mymoney-lkk.firebaseio.com/';
+const baseURL = 'https://mymoney-alonsosistemas.firebaseio.com/'
 const {useGet} = Rest(baseURL);
+
 
 
 
 const Movimentacoes = ({match}) => {
   const data = useGet(`movimentacoes/${match.params.data}`);
-
-
   return (
       <div className='container'>
         <h1>Movimentações</h1>       
@@ -22,21 +23,20 @@ const Movimentacoes = ({match}) => {
             </tr>
           </thead>
           <tbody>
-            { 
-                data.data &&
-                Object
-                .keys(data.data)
-                .map(movimentacao => {
-                  return (
-                    <tr key={movimentacao}>
-                      <td>{data.data[movimentacao].descricao}</td>
-                      <td>{data.data[movimentacao].valor}</td>               
-                      
-                    </tr>
-                  )
-                })
-                }
-         
+            {
+              data.data &&
+              Object
+              .keys(data.data)
+              .map(movimentacao => {
+                return (
+                  <tr>
+                    <td>{data.data[movimentacao].descricao}</td>
+                    <td>{data.data[movimentacao].valor}</td>                    
+                  </tr>
+                )
+              })
+            }
+           
           </tbody>
         </table>
         <pre>{JSON.stringify(data)}</pre>
